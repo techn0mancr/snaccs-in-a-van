8b5594203a75215a2e7cd729811d022cb639469a
techn0mancr <17984241+techn0mancr@users.noreply.github.com>
2021-04-08 20:58:35 +1000

    Added currently-present routes.

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,13 +1,22 @@
+/* Import required libraries and types */
 import express = require("express");
-import {Request, Response} from "express";
+import {Router} from "express";
 
+/* Set up server app */
 const app = express();
 const port: number = +(process.env.PORT || 3000);
 
-app.get("/", (req: Request, res: Response) => {
-    res.send("Hello, world!");
-});
+/* Set up routes */
+const customerRouter: Router = require("./routes/customerRouter");
+const menuRouter: Router = require("./routes/menuRouter");
+const vendorRouter: Router = require("./routes/vendorRouter");
 
+/* Handle route requests */
+app.use("/customer", customerRouter);
+app.use("/menu", menuRouter);
+app.use("/vendor", vendorRouter);
+
+/* Listen for incoming connections */
 app.listen(port, () => {
     console.log("Backend listening on port " + port);
 });
