0995662548186d7576591bcb8d63272c5546ba78
techn0mancr <17984241+techn0mancr@users.noreply.github.com>
2021-04-15 01:20:11 +1000

    Hotfixed MongoDB server connection issues.

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,15 +2,18 @@
 import express from "express";
 import { Router } from "express";
 
+/* Connect to the database */
+require("./models");
+
 /* Set up server app */
 const app = express();
 const port: number = +(process.env.PORT || 3000);
 
 /* Register routes */
 import routes from "./routes";
-app.use("/", routes);
+app.use("/api", routes);
 
 /* Listen for incoming connections */
 app.listen(port, () => {
-    console.log("Backend listening on port " + port);
+    console.log(`API listening on port ${port}.`);
 });
