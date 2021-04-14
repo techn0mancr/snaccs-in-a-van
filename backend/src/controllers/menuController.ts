0c12a5ca37fd9768ea4c085a5c9df21fa5532cda
techn0mancr <17984241+techn0mancr@users.noreply.github.com>
2021-04-15 02:25:08 +1000

    Implemented basic menu retrieval.

--- backend/src/controllers/menuController.ts
+++ backend/src/controllers/menuController.ts
@@ -1,13 +1,16 @@
-
 /* Import required types */
 import { Request, Response } from "express";
 
+/* Import required models */
+import { Menu } from "../models";
+
 /* Gets the menu from a specific van */
-function getMenu(req: Request & {params: {vendorID: string}}, res: Response): void {
-    
+async function getMenu(req: Request & {params: {vendorID: string}}, res: Response): Promise<void> {
+    const menus = await Menu.find({}).populate("items.itemId", "name price mimetype");
+    res.send(menus);
 }
 
-function getItemDetails(req: Request & {params: {itemID: string}}, res: Response): void {
+async function getItemDetails(req: Request & {params: {itemID: string}}, res: Response): Promise<void> {
 
 }
 
