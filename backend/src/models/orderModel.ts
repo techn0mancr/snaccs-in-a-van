2bdad98a36630d8c3b22d07717b81a7285927831
proguy18 <josephjoey.leonardi@gmail.com>
2021-04-19 02:17:53 +0700

    split orderModel into 2 seperate schemas

--- backend/src/models/orderModel.ts
+++ backend/src/models/orderModel.ts
@@ -1,81 +1,68 @@
 /* Import the required libraries and types */
-import { Document, model, Model, Schema } from "mongoose";
+import { Document, model, Model, Mongoose, Schema } from "mongoose";
+import { IItemOrder } from "./orderItemModel";
+/* Import required models */
 
 /* Define the order status enum */
 enum OrderStatus {
-    Placed = "Placed",
-    Fulfilled = "Fulfilled",
-    Completed = "Completed"
+  Placed = "Placed",
+  Fulfilled = "Fulfilled",
+  Completed = "Completed",
 }
-
-/* Define the item order interface */
-export interface IItemOrder extends Document {
-    itemId: Schema.Types.ObjectId;
-    quantity: number;
-}
-
-/* Define the item order schema */
-const itemOrderSchema: Schema = new Schema({
-    itemId: {
-        type: Schema.Types.ObjectId,
-        ref: "Item",
-        required: true
-    },
-    quantity: {
-        type: Number,
-        min: 1,
-        required: true
-    }
-});
 
 /* Define the order interface */
 export interface IOrder extends Document {
-    status: OrderStatus;
-    items: Array<IItemOrder>;
-    total: number;
-    orderTimestamp: Date;
-    fulfilledTimestamp: Date;
-    isChanged: boolean;
-    isCancelled: boolean;
-    rating: number;
+  status: OrderStatus;
+  items: Array<IItemOrder>;
+  total: number;
+  orderTimestamp: Date;
+  fulfilledTimestamp: Date;
+  isChanged: boolean;
+  isCancelled: boolean;
+  rating: number;
 }
 
 /* Define the order schema */
 const orderSchema: Schema = new Schema({
-    status: {
-        type: String,
-        required: true
-    },
-    items: {
-        type: [itemOrderSchema],
-        required: true
-    },
-    total: {
-        type: Number,
-        required: true,
-        min: 0
-    },
-    orderTimestamp: {
-        type: Date,
-        default: Date.now
-    },
-    fulfilledTimestamp: {
-        type: Date,
-        default: null
-    },
-    isChanged: {
-        type: Boolean,
-        default: false
-    },
-    isCancelled: {
-        type: Boolean,
-        default: false
-    },
-    rating: {
-        type: Number,
-        min: 0,
-        max: 5
-    }
+  status: {
+    type: String,
+    required: true,
+  },
+  orderItems: {
+    type: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "OrderItem",
+      },
+    ],
+    required: true,
+  },
+  total: {
+    type: Number,
+    required: true,
+    min: 0,
+  },
+  orderTimestamp: {
+    type: Date,
+    default: Date.now,
+  },
+  fulfilledTimestamp: {
+    type: Date,
+    default: null,
+  },
+  isChanged: {
+    type: Boolean,
+    default: false,
+  },
+  isCancelled: {
+    type: Boolean,
+    default: false,
+  },
+  rating: {
+    type: Number,
+    min: 0,
+    max: 5,
+  },
 });
 
 /* Export the order model */
