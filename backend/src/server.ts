53272f3aed626afa09eeabbc3b90e2bc7fb359e8
proguy18 <josephjoey.leonardi@gmail.com>
2021-04-19 03:27:29 +0700

    Added welcome message to port 48080

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,6 @@
 /* Import required libraries and types */
 import express from "express";
-import { Router } from "express";
+import { Router, Request, Response } from "express";
 
 /* Connect to the database */
 require("./models");
@@ -8,9 +8,18 @@ require("./models");
 /* Set up server app */
 const app = express();
 const port: number = +(process.env.PORT || 48080);
+app.use(express.json()); //allows app to Json request body
+app.use(express.urlencoded({extended:true})); //allows app to read formdata type of request body
 
 /* Register routes */
 import routes from "./routes";
+
+app.get("/", (req: Request, res:Response) => {
+    res.status(200).json({
+        "message": "Welcome to snaccoverflow"
+    })
+})
+
 app.use("/api", routes);
 
 /* Listen for incoming connections */
