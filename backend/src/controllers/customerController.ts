320eaf7810bb10f69ddfe4b3d774d40ab3de18df
proguy18 <josephjoey.leonardi@gmail.com>
2021-04-19 17:27:42 +0700

    Added addSnackToOrder feature

--- backend/src/controllers/customerController.ts
+++ backend/src/controllers/customerController.ts
@@ -1,12 +1,34 @@
 /* Import required types */
-import {Request, Response} from "express";
+import { Request, Response } from "express";
+
+/* Import required models */
+import { Order } from "../models";
 
 /* Adds the given snack to the customer's order */
-function addSnackToOrder(req: Request, res: Response): void {
+/* Sends item and quantity (itemorder) and adds it to order */
+async function addSnackToOrder(
+  req: Request & { params: { itemId: string; quantity: number } },
+  res: Response
+): Promise<void> {
+  try {
+    
+    /* Create new order */
+    // var order = new Order(req.body);
+    // const orderSavedToDB = await order.save();
 
+    //Create new order via mongoose create method
+    const order = await Order.create(req.body);
+    res.status(201).json({
+      message: "Order created successfully",
+      order,
+    });
+    /* Fill out order schema */
+  } catch (err) {
+    res.status(501).json({
+      message: err.message,
+    });
+  }
 }
 
 /* Export functions */
-export {
-    addSnackToOrder
-};
+export { addSnackToOrder };
