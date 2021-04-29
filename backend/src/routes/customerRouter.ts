2edf3d2b72daa99a520441b2f5c58269c7f5cbe6
techn0mancr <17984241+techn0mancr@users.noreply.github.com>
2021-04-29 11:32:40 +1000

    Made all routes use a JSON parser middleware. Added routes for logging in and registering.

--- backend/src/routes/customerRouter.ts
+++ backend/src/routes/customerRouter.ts
@@ -8,8 +8,13 @@ const jsonParser = json();
 /* Import the customer controller */
 import * as controller from "../controllers/customerController";
 
+/* Use middlewares on the router */
+customerRouter.use(jsonParser);
+
 /* Handle customer routes */
-customerRouter.post("/order/add/:itemId", jsonParser, controller.addSnackToCart);
+customerRouter.post("/order/add/:itemId", controller.addSnackToCart);
+customerRouter.post("/login", controller.login);
+customerRouter.post("/register", controller.register);
 
 /* Export the router */
 export default customerRouter;
