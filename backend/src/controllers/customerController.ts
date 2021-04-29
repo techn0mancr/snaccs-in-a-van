00e2ca6363ec48d7865f58267b51129419a226e0
techn0mancr <17984241+techn0mancr@users.noreply.github.com>
2021-04-29 11:29:57 +1000

    Incompletely implemented functions for registering a new customer and logging in an existing customer.

--- backend/src/controllers/customerController.ts
+++ backend/src/controllers/customerController.ts
@@ -1,8 +1,9 @@
 /* Import required types */
+import { compareSync } from "bcrypt";
 import { Request, Response } from "express";
 
 /* Import required models */
-import { Customer, IItemOrder, ItemOrder } from "../models";
+import { Customer, ICustomer, IItemOrder, ItemOrder } from "../models";
 
 /* Adds the given snack, in the given quantity, to the customer's cart */
 async function addSnackToCart(req: Request & {
@@ -49,7 +50,60 @@ async function addSnackToCart(req: Request & {
     }
 }
 
+/* Logs a customer in */
+async function login(req: Request & {
+    body: { email: String, password: String }
+}, res: Response) {
+    try {
+        /* Check if a user with the given email exists */
+        const customer = await Customer.findOne(
+            {
+                email: req.body.email
+            }
+        );
+        if (customer && compareSync(req.body.password, customer.password)) {
+            res.status(400).send("Incorrect email/password!");
+        }
+        else {
+            /* Generate a token for the logged-in customer */
+            // TODO
+        }
+    }
+    catch (e) {
+        res.status(500).send(`Internal Server Error: ${e.message}`);
+    }
+}
+
+/* Registers a new customer */
+async function register(req: Request & {
+    body: { email: String, givenName: String, familyName: String, password: String }
+}, res: Response) {
+    try {
+        /* Insert a new customer into its collection */
+        const newCustomer: ICustomer = new Customer(
+            {
+                email: req.body.email,
+                givenName: req.body.givenName,
+                familyName: req.body.familyName,
+                password: req.body.password
+            }
+        );
+        await newCustomer.save();
+
+        /* Generate a token for the new customer */
+        // TODO
+
+        /* Send a response */
+        res.status(201).send("");
+    }
+    catch (e) {
+        res.status(500).send(`Internal Server Error: ${e.message}`);
+    }
+}
+
 /* Export controller functions */
 export {
-    addSnackToCart
+    addSnackToCart,
+    login,
+    register
 }
