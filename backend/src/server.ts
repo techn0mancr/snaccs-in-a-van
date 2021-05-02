ad900a28c89a59cd89b554ff3425e6b2cde2fdd6
techn0mancr <17984241+techn0mancr@users.noreply.github.com>
2021-05-03 03:47:48 +1000

    Configured the server to use sessions middleware and MongoDB as a session store.

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,13 @@
-/* Import required libraries and types */
+/* Import the required libraries and types */
+import connectMongoDBSession from "connect-mongodb-session"
 import express from "express";
 import { Router } from "express";
+import session from "express-session";
+import timestring from "timestring";
+
+/* Import the required constants */
+import { MONGODB_CONNECTION_STRING, MONGODB_DATABASE_NAME, SESSIONS_COLLECTION_NAME } from "./config";
+import { SESSIONS_SECRET } from "./secrets";
 
 /* Connect to the database */
 require("./models");
@@ -12,6 +19,23 @@ const port: number = +(process.env.PORT || 48080);
 /* Register routes */
 import routes from "./routes";
 app.use("/api", routes);
+
+/* Set up sessions */
+const MongoDBStore = connectMongoDBSession(session);
+app.use(session({
+    secret: SESSIONS_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    store: new MongoDBStore({
+        uri: MONGODB_CONNECTION_STRING,
+        databaseName: MONGODB_DATABASE_NAME,
+        collection: SESSIONS_COLLECTION_NAME
+    }),
+    cookie: {
+        maxAge: timestring("1d", "ms"),
+        secure: false
+    }
+}));
 
 /* Listen for incoming connections */
 app.listen(port, () => {
