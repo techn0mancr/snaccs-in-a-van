26347761efe7cabc8ff2fe54137854c607bc9fab
techn0mancr <17984241+techn0mancr@users.noreply.github.com>
2021-05-03 05:09:58 +1000

    Added route to log a customer out.

--- backend/src/routes/customerRouter.ts
+++ backend/src/routes/customerRouter.ts
@@ -1,5 +1,6 @@
 /* Import the required libraries and types */
 import { json, Router } from "express";
+import { customerAuth } from "../middlewares/authMiddleware";
 
 /* Set up the router */
 const customerRouter: Router = Router();
@@ -14,6 +15,7 @@ customerRouter.use(jsonParser);
 /* Handle customer routes */
 customerRouter.post("/order/add/:itemId", controller.addSnackToCart);
 customerRouter.post("/login", controller.login);
+customerRouter.get("/logout", customerAuth, controller.logout);
 customerRouter.post("/register", controller.register);
 
 /* Export the router */
