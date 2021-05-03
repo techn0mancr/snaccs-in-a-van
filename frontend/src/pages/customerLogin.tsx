08914ea67cbb3627acf0115329ebe2041e5e9f98
Livya Natasha Riany <livyanatasha.riany@student.unimelb.edu.au>
2021-05-04 00:21:49 +0700

    Minor fixes on login page UI

--- frontend/src/pages/customerLogin.tsx
+++ frontend/src/pages/customerLogin.tsx
@@ -3,8 +3,9 @@ import './customerLogin.css';
 
 function customerLogin() {
     const header = (
-        <div className="title">
-            <h1>Log In</h1>
+        <div className="titleLogin">
+            <h1 className="titleLog">Log In</h1>
+            <br />
             <h2>Don't have an account?</h2>
             <button className="signup" type="submit" value="signup"><h2> Sign up here</h2></button>
         </div>
@@ -14,7 +15,7 @@ function customerLogin() {
         <div className="containerLogin">
             <form method="POST" action="/login" id="form"> 
                 <input id="email" type="text" name="email" placeholder="email" required /><br/><br/>
-                <input id="password" type="password" name="password" placeholder="password" required /><br/><br/><br/><br/>
+                <input id="password" type="password" name="password" placeholder="password" required /><br/><br/>
                 <button className="login" type="submit" value="Login">Log in</button>
             </form>
         </div>
