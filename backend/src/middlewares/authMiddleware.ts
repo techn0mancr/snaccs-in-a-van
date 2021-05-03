2c6fe066036409a4e50dbdd89802aa07cd4c029a
techn0mancr <17984241+techn0mancr@users.noreply.github.com>
2021-05-04 02:45:35 +1000

    Added additional checks to catch sessions without userIds.

--- backend/src/middlewares/authMiddleware.ts
+++ backend/src/middlewares/authMiddleware.ts
@@ -5,16 +5,17 @@ import { Customer } from "../models";
 /* Middleware to authenticate a customer based on their session data */
 async function customerAuth(req: Request, res: Response, next: NextFunction) {
     /* Check if the session's userId field is populated */
-    if (req.session.userId) {
+    if (req.session.userId && req.session.userId != undefined) {
         /* Cast the ObjectIds */
         var castedCustomerId: undefined = (req.session.userId as unknown) as undefined;
 
         /* Verify that a customer with the customerId exists in the database */
-        if (await Customer.findById(castedCustomerId)) next();
+        const existingCustomer = await Customer.findById(castedCustomerId);
+        if (existingCustomer)
+            next();
     }
-    else {
-        res.status(403).send("Forbidden");
-    }
+    else
+        res.status(401).send("Unauthorized");
 }
 
 /* Export the authentication middlewares */
