04ee45582878b2199a02ae18fa73b4db3045c4a6
Livya Natasha Riany <livyanatasha.riany@student.unimelb.edu.au>
2021-05-05 02:49:52 +0700

    Fixedlogin button to link to next page

--- frontend/src/pages/login.tsx
+++ frontend/src/pages/login.tsx
@@ -3,12 +3,7 @@ import './profile.css';
 import { Link } from 'react-router-dom'
 import axios from 'axios';
 
-// const Login = () => {
 class Login extends React.Component {
-    // const [user, setUser] = useState({
-    //     email: "",
-    //     password: ""
-    // });
     state = {
         email: "",
         password: "",
@@ -29,33 +24,7 @@ class Login extends React.Component {
             }, (error) => {
                 console.log(error);
         });
-        // axios({
-        //     method: "post",
-        //     url: "http://localhost:48080/api/customer/login",
-        //     data: {user},
-        // })
-        // .then((response) => {
-        //     console.log(response);
-        //   }, (error) => {
-        //     console.log(error);
-        //   });
-
-        // fetch('http://localhost:48080/api/customer/login', {
-        //     method: 'POST',
-        //     body: JSON.stringify({ email, password }),
-        // })
-        // .then(res => res.json())
-        // .then(json => setUser(json.user))
-        // .then(response => response.json())
-
-        // e.preventDefault()
         }
-
-    
-
-    // const submit = (e: { preventDefault: () => void; }) => {
-    //     const Data = new FormData();
-    //     Data.append('email', {user.email});
 
         
         
@@ -76,11 +45,13 @@ class Login extends React.Component {
                 <form id="form" onSubmit={this.handleSubmit}> 
                     <input id="email" type="text" name="email" placeholder="email" value={email} onChange={this.handleChange} required /><br/><br/>
                     <input id="password" type="password" name="password" placeholder="password" value={password} onChange={this.handleChange} required /><br/><br/>
-                    {/* <input id="email" type="text" name="email" placeholder="email" value={user.email} onChange={e => setUser({ ...user, email: e.target.value })} required /><br/><br/>
-                    <input id="password" type="password" name="password" placeholder="password" value={user.password}  onChange={e => setUser({ ...user, password: e.target.value })} required /><br/><br/> */}
-                    {/* <Link to={"/profile/proof"}> */}
-                        <button className="login" type="submit">Log in</button>
-                    {/* </Link> */}
+                    
+                        <button className="login" type="submit">
+                            <Link to={"/profile/proof"}>
+                                <h2 className="click">Log in</h2>
+                            </Link>
+                        </button>
+                    
                 </form>
             </div>
         </div>
