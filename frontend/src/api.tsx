94b0ee4ab9163e70c018932e364adc71f5b64072
Livya Natasha Riany <livyanatasha.riany@student.unimelb.edu.au>
2021-05-05 11:35:48 +0700

    Added customer route in api.tsx

--- frontend/src/api.tsx
+++ frontend/src/api.tsx
@@ -4,8 +4,68 @@ import axios from 'axios';
 const BASE_URL = "http://localhost:48080/api";
 // const BASE_URL = "https://snaccs-in-a-van.herokuapp.com/api";
 
-
+/* get one order detail */
 function getOrderDetails(orderId: string) {
     const endpoint = `${BASE_URL}/order/${orderId}`;
     return axios.get(endpoint);
 } 
+
+function addItemToCart(itemId: String) {
+    const endpoint = `${BASE_URL}/customer/cart/add/${itemId}`;
+    return axios.post(endpoint, { itemId })
+            .then((response) => {
+                console.log(response);
+            }, (error) => {
+                console.log(error);
+        });
+}
+
+function checkoutCart() {
+    const endpoint = `${BASE_URL}/customer/cart/checkout`;
+    return axios.get(endpoint);
+}
+
+function emptyCart() {
+    const endpoint = `${BASE_URL}/customer/cart/clear`;
+    return axios.get(endpoint);
+}
+
+function getActiveOrders() {
+    const endpoint = `${BASE_URL}/customer/order/active`;
+    return axios.get(endpoint);
+}
+
+function getPastOrders() {
+    const endpoint = `${BASE_URL}/customer/order/past`;
+    return axios.get(endpoint);
+}
+
+function customerLogin(email: String, password: String) {
+    const endpoint = `${BASE_URL}/customer/login`;
+    return axios.post(endpoint, { email, password })
+            .then((response) => {
+                console.log(response);
+            }, (error) => {
+                console.log(error);
+        });
+}
+
+function customerLogout() {
+    const endpoint = `${BASE_URL}/customer/logout`;
+    return axios.get(endpoint);
+}
+
+function customerProfile() {
+    const endpoint = `${BASE_URL}/customer/profile`;
+    return axios.get(endpoint);
+}
+
+function customerRegister( email: String, givenName: String, familyName: String, password: String ) {
+    const endpoint = `${BASE_URL}/customer/login`;
+    return axios.post(endpoint, { email, givenName, familyName, password })
+            .then((response) => {
+                console.log(response);
+            }, (error) => {
+                console.log(error);
+        });
+}
