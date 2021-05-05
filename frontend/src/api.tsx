67f6b37c673d9c962ddc695679be6fe45cd46e1d
Livya Natasha Riany <livyanatasha.riany@student.unimelb.edu.au>
2021-05-05 11:49:29 +0700

    Implemented all routes in api

--- frontend/src/api.tsx
+++ frontend/src/api.tsx
@@ -4,12 +4,6 @@ import axios from 'axios';
 const BASE_URL = "http://localhost:48080/api";
 // const BASE_URL = "https://snaccs-in-a-van.herokuapp.com/api";
 
-/* get one order detail */
-function getOrderDetails(orderId: string) {
-    const endpoint = `${BASE_URL}/order/${orderId}`;
-    return axios.get(endpoint);
-} 
-
 function addItemToCart(itemId: String) {
     const endpoint = `${BASE_URL}/customer/cart/add/${itemId}`;
     return axios.post(endpoint, { itemId })
@@ -63,6 +57,52 @@ function customerProfile() {
 function customerRegister( email: String, givenName: String, familyName: String, password: String ) {
     const endpoint = `${BASE_URL}/customer/login`;
     return axios.post(endpoint, { email, givenName, familyName, password })
+            .then((response) => {
+                console.log(response);
+            }, (error) => {
+                console.log(error);
+        });
+}
+
+function getMenu( vendorId: String ) {
+    const endpoint = `${BASE_URL}/menu/${vendorId}`;
+    return axios.get(endpoint);
+}
+
+function getItemDetails( itemId: String ) {
+    const endpoint = `${BASE_URL}/menu/${itemId}`;
+    return axios.get(endpoint);
+}
+
+/* get one order detail */
+function getOrderDetails( orderId: String ) {
+    const endpoint = `${BASE_URL}/order/${orderId}`;
+    return axios.get(endpoint);
+}
+
+function fulfillOrder( orderId: String ) {
+    const endpoint = `${BASE_URL}/order/${orderId}/fulfill`;
+    return axios.get(endpoint);
+}
+
+function getOutstandingOrders( vendorId: String ) {
+    const endpoint = `${BASE_URL}/vendor/${vendorId}/order/outstanding`;
+    return axios.get(endpoint);
+}
+
+function setVendorLocation( vendorId: String ) {
+    const endpoint = `${BASE_URL}/vendor/${vendorId}/update/location`;
+    return axios.post(endpoint, { vendorId })
+            .then((response) => {
+                console.log(response);
+            }, (error) => {
+                console.log(error);
+        });
+}
+
+function setVendorAvailability( vendorId: String ) {
+    const endpoint = `${BASE_URL}/vendor/${vendorId}/update/status`;
+    return axios.post(endpoint, { vendorId })
             .then((response) => {
                 console.log(response);
             }, (error) => {
