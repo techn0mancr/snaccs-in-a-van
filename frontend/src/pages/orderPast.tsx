73ed71ff85dcfe59b4607e82bfc4431eb69b3674
Livya Natasha Riany <livyanatasha.riany@student.unimelb.edu.au>
2021-05-06 12:01:34 +0700

    Implemet order past to take information from API

--- frontend/src/pages/orderPast.tsx
+++ frontend/src/pages/orderPast.tsx
@@ -3,35 +3,79 @@ import './order.css';
 import leftArrow from "../img/leftArrow.png"
 import rightArrow from "../img/rightArrow.png"
 import { Link } from 'react-router-dom'
+import axios from "axios";
 
-function orderCurrent() {
-    const header = (
-        <div className="title">
-            <br /><br />
-            <input type="image" alt="back" className="back" src={leftArrow} />
-            <h1>Past Orders</h1>
-        </div>
-    )
+class Header extends React.Component {
+    render() {
+        return (
+            <div className="title">
+                <br /><br />
+                <input type="image" alt="back" className="back" src={leftArrow} />
+                <h1>Past Orders</h1>
+            </div>
+        )
+    }
+}
+       
+class ListPastOrder extends React.Component {
+    state = {
+        error: null,
+        isLoaded: false,
+        orderList: [] as any[]
+    }
 
-    const content = (
-        <div className="content">
-            <Link to={'/cart/checkout'}>
-                <button className="order" type="submit" id="done" value="order">
-                    <img alt="right arrow" className="right" id="dark" src={rightArrow} />
-                    <h2>Tasty Trailer</h2>
-                    <p>Preparing order</p>
-                    <p className="date">29 April 2021 3.30 PM</p>
-                </button>
-            </Link>
-        </div>
-    )
+    async getPastOrders() {
+        const BASE_URL = "http://localhost:48080/api";
+        const endpoint = `${BASE_URL}/customer/order/past`;
+        return await axios.get(endpoint)
+        .then((response) => {
+            var data = response.data
+            this.setState({isLoaded: true, orderList: data});
+            console.log(response);
+        }, (error) => {
+            this.setState({isLoaded: true, error});
+            console.log(error);
+        },);
+    }
 
-    return (
-        <div>
-            {header}
-            {content}
-        </div>
-    )
+    componentDidMount() {
+        this.getPastOrders();
+    }
+
+    render() {
+        const { error, isLoaded, orderList } = this.state;
+
+        return (
+            <div className="content">
+                { orderList.length ?
+                    orderList.map((order, i) => (   
+                        <div key={i}>
+                            <button className="order" type="submit" value="order">
+                                <img alt="right arrow" className="right" src={rightArrow} />
+                                <h2>{order.vendorId}</h2>
+                                <p id="ready">{order.status}</p>
+                                <p className="date">{order.placedTimestamp}</p>
+                            </button>
+                        </div> 
+                        
+                ))
+            :
+            (<h2>No Order Present</h2>)
+            }
+            </div>
+        )
+    }
 }
 
-export default orderCurrent;
+class OrderPast extends React.Component {
+    render() {
+        return (
+            <div>
+                <Header />
+                <ListPastOrder />
+            </div>
+        )
+    }
+}
+        
+export default OrderPast
