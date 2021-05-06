ebe4b2ddc7a435547895c3d3d7f8b3beb5844b72
proguy18 <josephjoey.leonardi@gmail.com>
2021-05-07 01:36:45 +0700

    itemCard component and menu page template

---
+++ frontend/src/pages/menu.tsx
@@ -0,0 +1,13 @@
+// import ItemCounter from "../components/itemCounter";
+import AddToCartButton from "../components/addToCartButton";
+
+const Menu = () => {
+  return (
+    <div>
+      {/* <ItemCounter /> */}
+      <AddToCartButton />
+    </div>
+  );
+};
+
+export default Menu;
