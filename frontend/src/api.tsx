9846bd967ab0954c65f3b111317db8b563001fe1
jeffrey-kolenchery <jeffrey.kolenchery@gmail.com>
2021-05-07 15:52:35 +0400

    fixed urls to access herokuapp

--- frontend/src/api.tsx
+++ frontend/src/api.tsx
@@ -2,8 +2,8 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import history from "./history";
 
-const BASE_URL = "http://localhost:48080/api";
-// const BASE_URL = "https://snaccs-in-a-van.herokuapp.com/api";
+// const BASE_URL = "http://localhost:48080/api";
+const BASE_URL = "https://snaccs-in-a-van.herokuapp.com/api";
 
 function getCart() {
     const endpoint = `${BASE_URL}/customer/cart`;
