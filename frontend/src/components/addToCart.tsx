a5a2babdff98aa57d7f0e0364b0c8ae0f3ce85de
proguy18 <josephjoey.leonardi@gmail.com>
2021-05-07 20:54:05 +0700

    changes

--- frontend/src/components/addToCart.tsx
+++ frontend/src/components/addToCart.tsx
@@ -3,12 +3,11 @@ import ButtonGroup from "@material-ui/core/ButtonGroup";
 import Button from "@material-ui/core/Button";
 import AddIcon from "@material-ui/icons/Add";
 import RemoveIcon from "@material-ui/icons/Remove";
-import leftArrow from "../img/leftArrow.png"
+import leftArrow from "../img/leftArrow.png";
 import { addItemToCart, getItemDetails } from "../api";
 import "../css/addToCart.css";
-import history from '../history';
-import { customerProfile } from '../api';
-
+import history from "../history";
+import { customerProfile } from "../api";
 
 export default function AddToCart() {
   // const itemID = "607073f83ed89dee65af788d";
@@ -29,8 +28,8 @@ export default function AddToCart() {
   };
 
   function getId() {
-    const query = history.location.search
-    const id = query.replace('?id=','')
+    const query = history.location.search;
+    const id = query.replace("?id=", "");
     return id;
   }
 
@@ -73,52 +72,64 @@ export default function AddToCart() {
 
   return (
     <div>
-        <div className="fixed-top add-card">
-          <h1 className="cart-h1">Add to Cart</h1>
-          <img
-            className="cart-img card"
-            id="base64image"
-            src={`data:${getItem.mimetype};base64,${getItem.data}`}
-            alt={getItem.name}
-          />
+      <div className="fixed-top add-card">
+        <h1 className="cart-h1">Add to Cart</h1>
+        <img
+          className="cart-img card"
+          id="base64image"
+          src={`data:${getItem.mimetype};base64,${getItem.data}`}
+          alt={getItem.name}
+        />
 
-          <div className="add-container">
-            <button type="button" className="cart-button" onClick={() => add(itemID, itemCount)}>
-              Add to Cart
-            </button>
-            <h2 className="cart-h2">{getItem.name}</h2>
-            <br></br>
-            <h3 className="cart-h3">${toTwoDecimalPlaces(getItem.price)}</h3>
-            <div className="number">
-              <ButtonGroup>
-                <Button
-                  onClick={() => {
-                    setItemCount(Math.max(itemCount - 1, 0));
-                  }}
-                >
-                  {" "}
-                  <RemoveIcon fontSize="small" />
-                </Button>
-                <input
-                  className="quantity-input__screen"
-                  type="text"
-                  value={itemCount}
-                  readOnly
-                />
-                <Button
-                  onClick={() => {
-                    setItemCount(itemCount + 1);
-                  }}
-                >
-                  {" "}
-                  <AddIcon fontSize="small" />
-                </Button>
-                {/* <Badge color="secondary" badgeContent={itemCount}></Badge> */}
-              </ButtonGroup>
-            </div>
+        <div className="add-container">
+          <button
+            type="button"
+            className="cart-button"
+            onClick={() => add(itemID, itemCount)}
+          >
+            Add to Cart
+          </button>
+          <h2 className="cart-h2">{getItem.name}</h2>
+          <br></br>
+          <h3 className="cart-h3">
+            ${toTwoDecimalPlaces(getItem.price * itemCount)}
+          </h3>
+          <div className="number">
+            <ButtonGroup>
+              <Button
+                onClick={() => {
+                  setItemCount(Math.max(itemCount - 1, 0));
+                }}
+              >
+                {" "}
+                <RemoveIcon fontSize="small" />
+              </Button>
+              <input
+                className="quantity-input__screen"
+                type="text"
+                value={itemCount}
+                readOnly
+              />
+              <Button
+                onClick={() => {
+                  setItemCount(itemCount + 1);
+                }}
+              >
+                {" "}
+                <AddIcon fontSize="small" />
+              </Button>
+              {/* <Badge color="secondary" badgeContent={itemCount}></Badge> */}
+            </ButtonGroup>
           </div>
         </div>
-        <input type="image" alt="back" className="back" src={leftArrow} onClick={()=> history.goBack()}/>
+      </div>
+      <input
+        type="image"
+        alt="back"
+        className="back"
+        src={leftArrow}
+        onClick={() => history.goBack()}
+      />
     </div>
   );
 }
