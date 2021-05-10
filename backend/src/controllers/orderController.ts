ada93b177e676f72de926089f90228cf5b87ac97
techn0mancr <17984241+techn0mancr@users.noreply.github.com>
2021-05-11 02:36:53 +1000

    Moved fulfillOrder() to the vendor controller, and added an additional vendorId check such that only the logged-in vendor can mark their own order as 'Fulfilled'.

--- backend/src/controllers/orderController.ts
+++ backend/src/controllers/orderController.ts
@@ -4,40 +4,6 @@ import { Request, Response } from "express";
 /* Import required models */
 import { Order, OrderStatus } from "../models";
 
-/* Sets the status of a given order to "Fulfilled" */
-async function fulfillOrder(req: Request & {params: {orderId: string}}, res : Response): Promise<void> {
-    try {
-        /* Cast the ObjectIds */
-        var castedOrderId: undefined = (req.params.orderId as unknown) as undefined;
-        
-        /* Query the database */
-        const qResult = await Order.updateOne(
-            {
-                _id: castedOrderId
-            },
-            {
-                $set: {
-                    status: OrderStatus.Fulfilled,
-                    fulfilledTimestamp: new Date()
-                }
-            }
-        );
-        
-        /* Check if the query has successfully executed */
-        if (qResult.ok == 1) {
-            if (qResult.n > 0 && qResult.nModified > 0 && qResult.n == qResult.nModified)
-                res.status(200).send("OK");
-            else
-                res.status(404).send("Not Found");
-        }
-        else
-            res.status(500).send("Internal Server Error");
-    }
-    catch (e) {
-        res.status(500).send(`Internal Server Error: ${e.message}`);
-    }
-}
-
 /* Returns the details of an order */
 async function getOrderDetails(req: Request & {
     params: { orderId: string }
@@ -81,6 +47,5 @@ async function getOrderDetails(req: Request & {
 
 /* Export controller functions */
 export {
-    fulfillOrder,
     getOrderDetails
 }
--- backend/src/controllers/vendorController.ts
+++ backend/src/controllers/vendorController.ts
@@ -8,6 +8,42 @@ import {
     Vendor
 } from "../models";
 
+/* Sets the status of a given order to "Fulfilled" */
+async function fulfillOrder(req: Request & {
+    params: { orderId: string }
+}, res : Response): Promise<void> {
+    try {
+        /* Cast the ObjectIds */
+        var castedOrderId: undefined = (req.params.orderId as unknown) as undefined;
+        
+        /* Query the database */
+        const qResult = await Order.updateOne(
+            {
+                _id: castedOrderId,
+                vendorId: req.session.vendorId
+            },
+            {
+                $set: {
+                    status: OrderStatus.Fulfilled,
+                    fulfilledTimestamp: new Date()
+                }
+            }
+        );
+        
+        /* Check if the query has successfully executed */
+        if (qResult.ok == 1) {
+            if (qResult.n > 0 && qResult.nModified > 0 && qResult.n == qResult.nModified)
+                res.status(200).send("OK");
+            else
+                res.status(404).send("Not Found");
+        }
+        else
+            res.status(500).send("Internal Server Error");
+    }
+    catch (e) {
+        res.status(500).send(`Internal Server Error: ${e.message}`);
+    }
+}
 /* Get the given vendor's outstanding orders */
 async function getOutstandingOrders(req: Request, res: Response): Promise<void> {
     try {
@@ -157,6 +193,7 @@ async function setVendorLocation(req: Request & {
 
 /* Export controller functions */
 export {
+    fulfillOrder,
     getOutstandingOrders,
     login,
     logout,
