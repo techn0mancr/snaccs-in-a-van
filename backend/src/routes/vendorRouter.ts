66cfda7c823f884ecafde915acb6536ffb3d65a5
techn0mancr <17984241+techn0mancr@users.noreply.github.com>
2021-05-11 02:26:35 +1000

    Added routes to login and logout a vendor. Changed the HTTP request types of most routes, and made them use authentication middleware.

--- backend/src/routes/vendorRouter.ts
+++ backend/src/routes/vendorRouter.ts
@@ -1,5 +1,6 @@
 /* Import the required libraries and types */
 import { json, Router } from "express";
+import { vendorAuth } from "../middlewares/authMiddleware";
 
 /* Set up the router */
 const vendorRouter: Router = Router();
@@ -8,10 +9,16 @@ const jsonParser = json();
 /* Import the vendor controller */
 import * as controller from "../controllers/vendorController";
 
-/* Handle vendor routes */
-vendorRouter.get("/:vendorId/orders/outstanding", controller.getOutstandingOrders);
-vendorRouter.post("/:vendorId/update/location", jsonParser, controller.setVendorLocation);
-vendorRouter.post("/:vendorId/update/status", jsonParser, controller.setVendorAvailability);
+/* Use middlewares on the router */
+vendorRouter.use(jsonParser);
+
+/* Handle vendor routes at /api/vendor/... */
+vendorRouter.patch("/login", controller.login);
+vendorRouter.patch("/logout", vendorAuth, controller.logout);
+vendorRouter.get("/orders/outstanding", vendorAuth, controller.getOutstandingOrders);
+vendorRouter.patch("/update/location", vendorAuth, controller.setVendorLocation);
+vendorRouter.patch("/update/status", vendorAuth, controller.setVendorAvailability);
+
 
 /* Export the router */
 export default vendorRouter;
