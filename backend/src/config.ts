476dac3a3c342072759580bee9950861d33985cc
techn0mancr <17984241+techn0mancr@users.noreply.github.com>
2021-05-13 05:12:16 +1000

    Added constants to dictate when to give late fulfillment discounts, and how much of a discount it should be.

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -1,5 +1,8 @@
+/* Import required libraries and types */
+import timestring from "timestring";
 import { MONGODB_USERNAME, MONGODB_PASSWORD } from "./secrets";
 
+/* MongoDB-related constants */
 const MONGODB_DATABASE_NAME = "snaccsInAVan";
 const MONGODB_CONNECTION_STRING = `mongodb+srv://${MONGODB_USERNAME}:${MONGODB_PASSWORD}@snaccs-in-a-van.4ciyf.mongodb.net/snaccsInAVan?retryWrites=true&w=majority`;
 const MONGODB_CONNECTION_OPTIONS = {
@@ -10,9 +13,16 @@ const MONGODB_CONNECTION_OPTIONS = {
 }
 const SESSIONS_COLLECTION_NAME = "sessions";
 
+/* Application-related constants */
+const LATE_FULFILLMENT_ELAPSED_TIME = timestring("15mins", "ms");
+const LATE_FULFILLMENT_DISCOUNT = 0.2;
+
 export {
     MONGODB_CONNECTION_STRING,
     MONGODB_CONNECTION_OPTIONS,
     MONGODB_DATABASE_NAME,
-    SESSIONS_COLLECTION_NAME
+    SESSIONS_COLLECTION_NAME,
+    
+    LATE_FULFILLMENT_ELAPSED_TIME,
+    LATE_FULFILLMENT_DISCOUNT
 }
