578c4df7f00d41fcafd8226f322f7a9dd603abd6
Callista Li Yin Low <callistaliyin.low@student.unimelb.edu.au>
2021-05-22 01:05:45 +0800

    Changed styling of the components and time format

--- frontend/src/pages/vendorOrder.tsx
+++ frontend/src/pages/vendorOrder.tsx
@@ -110,10 +110,10 @@ class Content extends React.Component {
                             <div key={i}>
                                 <div className ="perOrder" onClick={() => this.handleDisplay(order._id, true)}>
                                     <div className ="leftBox">
-                                        <p className = "vendorOrder">{order._id}</p>
-                                        <p className = "vendorOrder">{moment(order.placedTimestamp).format('D MMM YYYY h.mm A')}</p>
+                                    <p className = "p-vendorOrder">{order._id}</p>
+                                    <p className = "p-vendorOrder">{moment(order.placedTimestamp).format('h.mm A')}</p>
                                     </div>
-                                    <p className = "orderName">{order.customerId.givenName} {order.customerId.familyName}</p>
+                                    <p className = "p-orderName">{order.customerId.givenName} {order.customerId.familyName}</p>
                                 </div>
                             </div>
                         ))}
@@ -148,10 +148,10 @@ class Content extends React.Component {
                             <div key={i}>
                                 <div className="perOrder" onClick={() => this.handleDisplay(fulfill._id, false)}>
                                     <div className="leftBox">
-                                        <p className = "vendorOrder">{fulfill._id}</p>
-                                        <p className = "vendorOrder">{moment(fulfill.placedTimestamp).format('D MMM YYYY h.mm A')}</p>
+                                        <p className = "p-vendorOrder">{fulfill._id}</p>
+                                        <p className = "p-vendorOrder">{moment(fulfill.placedTimestamp).format('h.mm A')}</p>
                                     </div>
-                                    <p className = "orderName">{fulfill.customerId.givenName} {fulfill.customerId.familyName}</p>
+                                    <p className = "p-orderName">{fulfill.customerId.givenName} {fulfill.customerId.familyName}</p>
                                     <button type="button" className="btn-vendorOrder" onClick={() => this.handleComplete(fulfill._id)}>Completed</button>
                                 </div>
                             </div>
