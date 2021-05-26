d47e8f6b189c8d3431340930d78e88d7cc57733c
Callista Li Yin Low <callistaliyin.low@student.unimelb.edu.au>
2021-05-27 00:40:11 +0800

    Change menu background

--- frontend/src/pages/nearest.tsx
+++ frontend/src/pages/nearest.tsx
@@ -50,7 +50,7 @@ class ListNearest extends React.Component {
                                             <h3 className="nVan">{vendor.locationDescription}</h3>
                                             <p className="nVan">{getDistance([window.sessionStorage.getItem("lat") as unknown as number, 
                                             window.sessionStorage.getItem("lng") as unknown as number],
-                                             vendor.geolocation)} kms away...</p>
+                                             vendor.geolocation)} km away from you</p>
                                             <i className="fas fa-chevron-right"></i>
                                         </div>
                                     {/* </div> */}
