aded831b42cae9efe72c220f190714f948d761b5
Livya Natasha Riany <livyanatasha.riany@student.unimelb.edu.au>
2021-05-30 00:27:13 +0700

    Minor change on get coordinate from customer

--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -5,9 +5,15 @@ import React from 'react';
 import "./home.css";
 import history from '../history';
 import snaccVan from '../img/food-van.svg';
+import { getCustomerGeolocation } from '../api';
 
 /* Header component of Home Page */
 class Home extends React.Component {
+
+    handleClick() {
+        getCustomerGeolocation();
+        history.push(`/menu`);
+    }
     render() {
         return (
             <div className="titleLogin">
@@ -16,7 +22,7 @@ class Home extends React.Component {
                 </span>
                 <h2 className="home">I am a ...</h2>
                 <span className="landingbtn">
-                    <button className="home" type="button" onClick={() => history.push(`/menu`)}>Customer</button>
+                    <button className="home" type="button" onClick={() => this.handleClick()}>Customer</button>
                     <button className="login" type="button" onClick={() => history.push(`/vendor/login`)}>Vendor</button>
                 </span>
             </div>
