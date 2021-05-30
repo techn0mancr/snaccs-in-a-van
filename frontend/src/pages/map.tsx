cfefd94472a609d5e0a002e88fa679c42f58e90a
Livya Natasha Riany <livyanatasha.riany@student.unimelb.edu.au>
2021-05-30 14:56:34 +0700

    Added comments

--- frontend/src/pages/map.tsx
+++ frontend/src/pages/map.tsx
@@ -1,6 +1,5 @@
 /* Import the required libraries and types */
 import React from 'react';
-import mapboxgl from 'mapbox-gl';
 import * as L from 'leaflet';
 import 'mapbox-gl-leaflet';
 
@@ -31,6 +30,7 @@ interface MainProps {
 
 /* Render all components on Map Page */
 class Content extends React.Component<MainProps, any> {
+
     constructor(props: MainProps) {
         super(props);
         this.state = {
@@ -42,10 +42,14 @@ class Content extends React.Component<MainProps, any> {
         }
     }
 
+    /* During on page, re-render every second */
     componentDidMount() {
         const { lat, lng, zoom } = this.state;
 
+        /* Generate map into state */
         var map = L.map(this.state.mapContainer.current).setView([lat, lng], zoom);
+
+        /* Customize icon */
         var vanIcon = new L.Icon({
             iconUrl: vanPoint,
             iconSize: [40, 41],
@@ -59,17 +63,22 @@ class Content extends React.Component<MainProps, any> {
             popupAnchor: [1, -34],
         });
         
+        /* Pin point marker for current location */
         L.marker([lat, lng], {icon: pinIcon})
         .bindPopup("You are here!")
         .addTo(map)
         .openPopup();
 
+        /* Get information of closest vendors */
         getVendors().then(
             (response) => {
                 var data = response.data
                 this.setState({vendors: data})
+                /* Generate markers for each individual vendor */
                 var i = 0 ;
                 while (i < 5) {
+
+                    /* Pin point marker for vendor's location and link to corresponding menu page */
                     L.marker([data[i].geolocation[0], data[i].geolocation[1]], {icon: vanIcon})
                     .bindPopup(data[i].name +  "<br/>" + data[i].locationDescription + "<br/><a href=/menu/vendor/?id=" + data[i]._id + ">Order</a>" )
                     .addTo(map)
@@ -80,7 +89,8 @@ class Content extends React.Component<MainProps, any> {
                 console.log(error);
             }
         )
-
+        
+        /* Style the map using mapbox */
         L.mapboxGL({
             accessToken: 'pk.eyJ1IjoibGl2eWFuYXRhc2hhIiwiYSI6ImNrcGE0b2ttajBrY3kycGxsdnRxNXB5dmgifQ.ZU7MvBafKMb1fX91QcItiQ',
             style: 'mapbox://styles/livyanatasha/ckpa4turt3glb18qtp0d6qz4r'
