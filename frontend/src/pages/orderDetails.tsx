4e9e09cabff252b41ff3f80609d554ac8f205c6e
proguy18 <josephjoey.leonardi@gmail.com>
2021-06-06 18:09:14 +0700

    Removed useless information displayed in a cancelled order

--- frontend/src/pages/orderDetails.tsx
+++ frontend/src/pages/orderDetails.tsx
@@ -13,6 +13,13 @@ moment().format();
 const currencyOptions = {
   minimumFractionDigits: 2,
   maximumFractionDigits: 2,
+};
+
+enum OrderStatus {
+  Placed = "Placed",
+  Fulfilled = "Fulfilled",
+  Completed = "Completed",
+  Cancelled = "Cancelled",
 }
 
 /* Return number into 2 decimal places */
@@ -21,22 +28,23 @@ function toTwoDecimalPlaces(number: number) {
 }
 
 /* Header component of Order Details Page */
-class Header extends React.Component {
-  render() {
-    return (
-      <div className="title">
-        <br></br>
-        <input type="image" alt="back" className="back" src={leftArrow} onClick={() => history.goBack()}/>
-        <h1>Order</h1>
-        <br></br>
-      </div>
-    )
-  }
-}
+const Header = () => (
+  <div className="title">
+    <br></br>
+    <input
+      type="image"
+      alt="back"
+      className="back"
+      src={leftArrow}
+      onClick={() => history.goBack()}
+    />
+    <h1>Order</h1>
+    <br></br>
+  </div>
+);
 
 /* Content component of Order Details Page */
 class Information extends React.Component {
-
   state = {
     error: null,
     details: [] as any,
@@ -49,8 +57,9 @@ class Information extends React.Component {
     paidAmount: 0,
     discount: 0,
     isLoaded: false,
-    customer: false
-  }
+    customer: false,
+    status: "",
+  };
   orderId = getId() || "";
 
   /* Before rendering page */
@@ -61,56 +70,84 @@ class Information extends React.Component {
     /* Check customer already logged in */
     customerProfile().then(
       (response) => {
-          this.setState({customer: true});
-          console.log(response);
-        },
+        this.setState({ customer: true });
+        console.log(response);
+      },
       (error) => {
-          console.log(error);
+        console.log(error);
       }
-    )
+    );
   }
 
   /* During on page */
   componentDidMount() {
     this.checkDiscount();
-  }  
+  }
 
   /* Get order details from api */
   orderDetails(orderId: String) {
     getOrderDetails(this.orderId).then(
       (response) => {
         var data = response.data;
-        this.setState({isLoaded: true, details: data, timeStamps: data.timestamps,vendorId: data.vendorId, items: data.items, rating: data.rating,paidAmount: data.total})
+        this.setState({
+          isLoaded: true,
+          details: data,
+          status: data.status,
+          timeStamps: data.timestamps,
+          vendorId: data.vendorId,
+          items: data.items,
+          rating: data.rating,
+          paidAmount: data.total,
+        });
         this.checkDiscount();
-      }, (error) => {
+      },
+      (error) => {
         this.setState({ isLoaded: true, error });
         console.log(error);
       }
-    )
+    );
   }
 
-   /* Check if there is discount if more than 15 minutes */
+  /* Check if there is discount if more than 15 minutes */
   checkDiscount() {
     const { timeStamps, paidAmount } = this.state;
     var fulfilled = moment(timeStamps.fulfilled);
     var placed = moment(timeStamps.placed);
     var duration = moment.duration(fulfilled.diff(placed));
 
-     /* Count the values if more than 15 minutes */
+    /* Count the values if more than 15 minutes */
     if (duration.minutes() > 15 || duration.hours() > 0) {
-      var totalAmount = paidAmount*1.25;
-      var discount = totalAmount-paidAmount;
-      this.setState({showDiscount: true, totalAmount: totalAmount, discount: discount});
+      var totalAmount = paidAmount * 1.25;
+      var discount = totalAmount - paidAmount;
+      this.setState({
+        showDiscount: true,
+        totalAmount: totalAmount,
+        discount: discount,
+      });
     } else {
-      this.setState({totalAmount: paidAmount});
+      this.setState({ totalAmount: paidAmount });
     }
   }
 
   render() {
-    const { showDiscount, error, details, vendorId, items, timeStamps, rating, paidAmount, totalAmount, discount, isLoaded, customer } = this.state;
+    const {
+      showDiscount,
+      error,
+      details,
+      vendorId,
+      items,
+      timeStamps,
+      rating,
+      paidAmount,
+      totalAmount,
+      discount,
+      isLoaded,
+      customer,
+      status,
+    } = this.state;
 
     if (error === true) {
-      return <h3 className ="error">fail</h3>;
+      return <h3 className="error">fail</h3>;
     } else if (isLoaded === false) {
       return (
         <div>
@@ -120,7 +157,7 @@ class Information extends React.Component {
             <h2 className="invoice">Loading...</h2>
           </div>
         </div>
-      )
+      );
     } else {
       return (
         <div>
@@ -130,17 +167,23 @@ class Information extends React.Component {
             <h2 className="invoice">
               {moment(timeStamps.completed).format("D MMM YYYY h.mm A")}
             </h2>
-            <br/>
-            { customer? 
+            <br />
+            {customer && status !== OrderStatus.Cancelled ? (
               <div>
-                {rating == null ?
-                  <button className="signup" type="submit" value="signup" onClick={() => history.push(`/order/rate/?id=${this.orderId}`)}><h2>Rate Order</h2></button>
-                  :null
-                }
+                {rating == null ? (
+                  <button
+                    className="signup"
+                    type="submit"
+                    value="signup"
+                    onClick={() =>
+                      history.push(`/order/rate/?id=${this.orderId}`)
+                    }
+                  >
+                    <h2>Rate Order</h2>
+                  </button>
+                ) : null}
               </div>
-            :null
-            }
-            
+            ) : <h2>Cancelled</h2>}
           </div>
 
           <div className="containerCheckout" id="loc">
@@ -165,36 +208,44 @@ class Information extends React.Component {
             ))}
           </div>
 
-          <div className="containerCheckout" id="payment">
-            <h2>Payment</h2>
+          {status !== OrderStatus.Cancelled && (
+            <div className="containerCheckout" id="payment">
+              <h2>Payment</h2>
 
-            <div className="amount">
-              <div className="item">
-              <p className="desc">Total amount</p>
-              </div>
-              <p className="price">${toTwoDecimalPlaces(totalAmount)}</p>
-            </div>
-            {showDiscount? 
               <div className="amount">
                 <div className="item">
-                <p className="desc">20% discount</p>
+                  <p className="desc">Total amount</p>
                 </div>
-                <p className="price">-${toTwoDecimalPlaces(discount)}</p>
+                <p className="price">${toTwoDecimalPlaces(totalAmount)}</p>
               </div>
-              :<div>
-                <br></br>
-                <br></br>
-              </div>
-            }
+              {showDiscount ? (
+                <div className="amount">
+                  <div className="item">
+                    <p className="desc">20% discount</p>
+                  </div>
+                  <p
+                    className="price"
+                    style={{ transform: "translateX(-8px)" }}
+                  >
+                    -${toTwoDecimalPlaces(discount)}
+                  </p>
+                </div>
+              ) : (
+                <div>
+                  <br></br>
+                  <br></br>
+                </div>
+              )}
 
-            <div className="amountPaid">
-            <div className="item">
-              <p className="desc">Amount to be paid</p>
-              </div>
+              <div className="amountPaid">
+                <div className="item">
+                  <p className="desc">Amount to be paid</p>
+                </div>
 
-              <p className="price">${toTwoDecimalPlaces(paidAmount)}</p>
+                <p className="price">${toTwoDecimalPlaces(paidAmount)}</p>
+              </div>
             </div>
-          </div>
+          )}
         </div>
       );
     }
@@ -202,15 +253,11 @@ class Information extends React.Component {
 }
 
 /* Render all components on Order Details Page */
-class OrderDetails extends React.Component {
-  render() {
-    return (
-      <div>
-        <Header />
-        <Information />
-      </div>
-    );
-  }
-}
+const OrderDetails = () => (
+  <div>
+    <Header />
+    <Information />
+  </div>
+);
 
 export default OrderDetails;
